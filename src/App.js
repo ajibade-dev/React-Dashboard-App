bda0085eb29c9a884d437475d419f8eb7cd53b6a
unknown <ajibadeifedolapo17@gmail.com>
2023-05-09 18:07:51 +0100

    Implemented the sidebar and theme functionality

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css'
 
 const App = () => {
-  const {activeMenu} = useStateContext();
+  const {activeMenu, themeSettings, setThemeSettings } = useStateContext();
   
   return (
     <div>
@@ -21,6 +21,7 @@ const App = () => {
           <div className='fixed right-4 bottom-4' style={{zIndex: '1000' }}>
             <TooltipComponent content="Settings" position='Top'>
               <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+              onClick={() => setThemeSettings(true)}
               style={{ background: 'blue', borderRadius: '50%' }}
               >
                 <FiSettings />
@@ -51,6 +52,7 @@ const App = () => {
 
               {/*Main Div for Routing*/}
             <div>
+             {themeSettings && <ThemeSettings />}
               <Routes>
                 {/* Dashboard */}
                 <Route path='/' element={<Ecommerce />} />
